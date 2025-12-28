e94a1307d054a13f7823f14b3b846c9f1039b266
Åsmund Hjulstad <ahju@equinor.com>
2025-12-28 12:11:27 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { fetchRoadEvents } from '$lib/service/roadevents.js';
+
+	const roadevents = fetchRoadEvents();
+	const MapView = browser ? import('./MapView.svelte') : null;
+
+	let { data } = $props();
+</script>
+
+{#if MapView}
+	{#await MapView then { default: Map }}
+		<Map {roadevents} />
+	{/await}
+{/if}
---
+++ src/routes/MapView.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Map, TileLayer, GeoJSON } from 'sveaflet';
+	import type { FeatureCollection, Feature } from 'geojson';
+	import type { Layer } from 'leaflet';
+
+	let { roadevents }: { roadevents: Promise<FeatureCollection> } = $props();
+
+	function onEachFeature(feature: Feature, layer: Layer) {
+		let popupContent = ``;
+		if (feature.properties?.DESCRIPTION) {
+			popupContent += feature.properties.DESCRIPTION;
+		}
+		layer.bindPopup(popupContent);
+	}
+</script>
+
+<div style="width:100%;height:80vh;">
+	<Map
+		options={{
+			center: [59.0546216, 5.6626464],
+			zoom: 9.5,
+			fadeAnimation: false
+		}}
+	>
+		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+		{#await roadevents then data}
+			<GeoJSON json={data} options={{ onEachFeature }} />
+		{/await}
+	</Map>
+</div>
