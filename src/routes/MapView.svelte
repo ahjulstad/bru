a8b0184047645693ef9245eaa7df7c1027fab8cb
Åsmund Hjulstad <ahju@equinor.com>
2025-12-29 18:44:40 +0000

    Refactor rainoverlay

--- src/routes/MapView.svelte
+++ src/routes/MapView.svelte
@@ -2,9 +2,11 @@
 	import { Map, TileLayer, GeoJSON } from 'sveaflet';
 	import type { FeatureCollection, Feature } from 'geojson';
 	import type { Layer } from 'leaflet';
-	import { onMount } from 'svelte';
+	import RainOverlay from './RainOverlay.svelte';
 
 	let { roadevents }: { roadevents: Promise<FeatureCollection> } = $props();
+
+	let timestamp = $state('');
 
 	function onEachFeature(feature: Feature, layer: Layer) {
 		let popupContent = ``;
@@ -13,55 +15,10 @@
 		}
 		layer.bindPopup(popupContent);
 	}
-
-	let images: string[] = $state([]);
-	let imagelength = $state(0);
-	let timestamps: string[] = $state([]);
-	let aFrame = $state(0);
-	let bFrame = $state(1);
-	let showb = $state(false);
-	let counter = $state(0);
-
-	onMount(async () => {
-		// Get list of animation map images
-		const overview = await fetch('https://beta.yr-maps.met.no/api/precipitation-nowcast/').then(
-			(res) => res.json()
-		);
-		images = overview.times.map((element: any) => element.tiles.webp);
-		timestamps = overview.times.map((element: any) =>
-			new Date(Date.parse(element.time)).toLocaleString(undefined, {
-				hour: '2-digit',
-				minute: '2-digit',
-				hour12: false
-			})
-		);
-		imagelength = images.length;
-	});
-
-	$effect(() => {
-		if (imagelength === 0) return;
-
-		const id = setInterval(() => {
-			counter += 1;
-			showb = !showb;
-			if (counter >= 6) {
-				clearInterval(id);
-			}
-			setTimeout(() => {
-				if (showb) {
-					aFrame = (bFrame + 1) % imagelength;
-				} else {
-					bFrame = (aFrame + 1) % imagelength;
-				}
-			}, 300);
-		}, 800);
-
-		return () => clearInterval(id);
-	});
 </script>
 
 <div>
-	<div class="h-[80vh] w-full">
+	<div class="h-[80vh] w-full bg-gray-200">
 		<Map
 			options={{
 				center: [59.0546216, 5.6626464],
@@ -70,39 +27,12 @@
 			}}
 		>
 			<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
-			{#if imagelength > 0}
-				<TileLayer
-					url={images[aFrame]}
-					opacity={showb ? 0 : 1}
-					options={{
-						className: 'radar-layer',
-						maxNativeZoom: 6
-					}}
-				/>
-				<TileLayer
-					url={images[bFrame]}
-					opacity={showb ? 1 : 0}
-					options={{
-						className: 'radar-layer',
-						maxNativeZoom: 6
-					}}
-				/>
-			{/if}
+			<RainOverlay bind:timestamp />
 			{#await roadevents then data}
 				<GeoJSON json={data} options={{ onEachFeature }} />
 			{/await}
 		</Map>
 	</div>
-	<div class="absolute bottom-5 left-4 z-800">{timestamps[aFrame]}</div>
+	<div class="absolute bottom-5 left-4 z-800">{timestamp}</div>
 </div>
 
-<style>
-	:global(.radar-layer),
-	:global(.radar-layer img) {
-		mix-blend-mode: screen;
-		transition: opacity 0.2s ease-in-out !important;
-		image-rendering: pixelated;
-		-webkit-backface-visibility: hidden;
-		backface-visibility: hidden;
-	}
-</style>
---
+++ src/routes/RainOverlay.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { TileLayer } from 'sveaflet';
+
+	let { timestamp = $bindable() } = $props();
+
+	let images: string[] = $state([]);
+	let imagelength = $state(0);
+	let timestamps: string[] = $state([]);
+	let aFrame = $state(0);
+	let bFrame = $state(1);
+	let showb = $state(false);
+	let counter = $state(0);
+
+	$effect(() => {
+		timestamp = timestamps[aFrame];
+	});
+
+	onMount(async () => {
+		// Get list of animation map images
+		const overview = await fetch('https://beta.yr-maps.met.no/api/precipitation-nowcast/').then(
+			(res) => res.json()
+		);
+		images = overview.times.map((element: any) => element.tiles.webp);
+		timestamps = overview.times.map((element: any) =>
+			new Date(Date.parse(element.time)).toLocaleString(undefined, {
+				hour: '2-digit',
+				minute: '2-digit',
+				hour12: false
+			})
+		);
+		imagelength = images.length;
+	});
+
+	$effect(() => {
+		if (imagelength === 0) return;
+
+		const id = setInterval(() => {
+			counter += 1;
+			showb = !showb;
+			if (counter >= 6) {
+				clearInterval(id);
+			}
+			setTimeout(() => {
+				if (showb) {
+					aFrame = (bFrame + 1) % imagelength;
+				} else {
+					bFrame = (aFrame + 1) % imagelength;
+				}
+			}, 300);
+		}, 800);
+
+		return () => clearInterval(id);
+	});
+</script>
+
+{#if imagelength > 2}
+	<TileLayer
+		url={images[aFrame]}
+		opacity={showb ? 0 : 1}
+		options={{
+			className: 'radar-layer',
+			maxNativeZoom: 6
+		}}
+	/>
+	{#if counter >= 1}
+		<TileLayer
+			url={images[bFrame]}
+			opacity={showb ? 1 : 0}
+			options={{
+				className: 'radar-layer',
+				maxNativeZoom: 6
+			}}
+		/>
+	{/if}
+{/if}
+
+<style>
+	:global(.radar-layer),
+	:global(.radar-layer img) {
+		mix-blend-mode: screen;
+		image-rendering: pixelated;
+		/* -webkit-backface-visibility: hidden;
+		backface-visibility: hidden; */
+	}
+</style>
