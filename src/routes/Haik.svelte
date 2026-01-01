7a9b4b5896a6dc85071cb738cf910eea359b4da8
Åsmund Hjulstad <ahju@equinor.com>
2026-01-01 16:23:24 +0000

    set fake password

--- src/routes/Haik.svelte
+++ src/routes/Haik.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { joinRoom } from 'trystero';
+	import type { Room } from 'trystero';
 
-	const config = { appId: 'bruhaik-neu4h57fh3kfadgkh73fhw' };
+	const config = {
+		appId: 'bruhaik-neu4h57fh3kfadgkh73fhw',
+		password: 'notsecret-only-here-to-test-it-is-SPA-after-all'
+	};
 
 	const onClick = () => {
 		console.log('Haik button clicked');
 	};
+	let room: Room;
 	onMount(() => {
-		const room = joinRoom(config, 'main-room');
+		room = joinRoom(config, 'main-room');
 		console.log(room.getPeers());
 		room.onPeerJoin((peerId) => {
 			console.log(`Peer joined: ${peerId}`);
 		});
+		room.onPeerLeave((peerId) => {
+			console.log(`Peer left: ${peerId}`);
+		});
+	});
+
+	onDestroy(() => {
+		room.leave();
 	});
 </script>
 
